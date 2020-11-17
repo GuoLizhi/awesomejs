75bcbb9c8aef52c1bb6af10eada41e0e5ecd290e
GuoLizhi <lizhi.guo@foxmail.com>
2020-11-17 20:52:09 +0800

    feat: add mvvm

--- build/gen-readme.js
+++ build/gen-readme.js
@@ -3,13 +3,13 @@ const path = require('path');
 
 function genReadme() {
   let result = '## 1.手写JavaScript系列\n\n| 索引 | 功能 | 地址 |\n|  ----  | ----  | ---- |\n';
-  const dirPath = path.resolve(__dirname, '../handwriting')
+  const dirPath = path.resolve(__dirname, '../HandWriting')
   let files = fs.readdirSync(dirPath);
   files.sort()
   files.map((file, index) => {
     const filePath = path.resolve(dirPath, file);
     const title = getSingleDescription(filePath);
-    result += `|${index+1}|${title}|[${file}](./handwriting/${file})|\n`;
+    result += `|${index+1}|${title}|[${file}](./HandWriting/${file})|\n`;
   });
   fs.writeFileSync(path.resolve(__dirname, '../README.md'), result);
   console.log('success!');
