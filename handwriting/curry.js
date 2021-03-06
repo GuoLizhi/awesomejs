b418d09dfa24a672d67955a67682dcc57ecd803b
GuoLizhi <lizhi.guo@foxmail.com>
2021-03-06 19:44:12 +0800

    add

--- handwriting/curry.js
+++ handwriting/curry.js
@@ -5,12 +5,13 @@
  */
 function curry(fn, args = []) {
   var length = fn.length;
+  var context = this
   return function () {
     newArgs = args.concat(Array.prototype.slice.call(arguments));
     // 如果当前函数的参数，小于总共所需要的参数
     // 则直接递归处理
     if (newArgs.length < length) {
-      return curry.call(this, fn, newArgs);
+      return curry.call(context, fn, newArgs);
     } else {
       return fn.apply(this, newArgs);
     }
