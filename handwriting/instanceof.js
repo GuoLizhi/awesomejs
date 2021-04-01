7fbd22533a772843b6617f65e650faeb6a2d5407
GuoLizhi <lizhi.guo@foxmail.com>
2021-04-01 11:22:14 +0800

    update: instanceOf

--- handwriting/instanceof.js
+++ handwriting/instanceof.js
@@ -1,5 +1,7 @@
 /**
  * instanceOf实现原理
+ * 核心原理：不断向上查找left的原型，与right的prototype进行对比。
+ * 如果期间遇到相等的那说明left就是right的实例。一旦left的prototype变为null，说明已经查找到原型链的顶端了，返回false
  * @param {Function} left 
  * @param {Function} right 
  */
@@ -9,6 +11,6 @@ function instanceOf (left, right) {
   while (true) {
     if (lProto === null) return false;
     if (lProto === rProto) return true;
-    lProto = rProto.__proto__;
+    lProto = lProto.__proto__;
   }
 }
